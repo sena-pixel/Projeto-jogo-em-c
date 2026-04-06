da851e8e503611558af8f4d787d113e680c1fba0
heitor <heitorsena925@gmail.com>
2026-04-05 22:32:22 -0300

    Atualizando codigo dos jogos

--- jogo/girotojogo.c
+++ jogo/girotojogo.c
@@ -7,116 +7,144 @@ void limparBuffer() {
 }
 
 void esperarEnter() {
-    printf("\nPressione ENTER para continuar...");
+    printf("\nPressione ENTER...");
     getchar();
 }
 
-void menu() {
+int menu() {
+    int op;
     printf("\n=== HUB DE JOGOS ===\n");
     printf("1. Perguntas\n");
     printf("2. Cobra na Caixa\n");
     printf("3. Gousmas War\n");
     printf("4. Sair\n");
     printf("Escolha: ");
+
+    if (scanf("%d", &op) != 1) {
+        limparBuffer();
+        return 0;
+    }
+    limparBuffer();
+    return op;
 }
 
-// JOGO 1
 void perguntas() {
-    int resposta;
-    char jogar;
+    int i, resp, jogar;
+
+    char *perguntas[5] = {
+        "7x8?",
+        "Capital da Franca?",
+        "2+2?",
+        "Cor do ceu?",
+        "Maior planeta?"
+    };
+
+    char *alt[5][4] = {
+        {"54","56","60","64"},
+        {"Berlim","Madrid","Paris","Roma"},
+        {"3","4","5","6"},
+        {"Azul","Verde","Vermelho","Amarelo"},
+        {"Terra","Marte","Jupiter","Lua"}
+    };
+
+    int corretas[5] = {2,3,2,1,3};
 
     do {
-        printf("7x8?\n1)54  2)56  3)64\n: ");
+        for(i=0;i<5;i++) {
+            printf("\n%s\n", perguntas[i]);
+            printf("1)%s 2)%s 3)%s 4)%s\n",
+                alt[i][0], alt[i][1], alt[i][2], alt[i][3]);
 
-        while (1) {
-            if (scanf("%d", &resposta) != 1) {
-                printf("Invalido!\n");
-                limparBuffer();
-                continue;
+            while(1) {
+                if(scanf("%d",&resp)!=1){
+                    printf("Invalido!\n");
+                    limparBuffer();
+                    continue;
+                }
+                if(resp<1 || resp>4){
+                    printf("Digite 1 a 4\n");
+                    continue;
+                }
+                break;
             }
-            if (resposta < 1 || resposta > 3) {
-                printf("Digite 1, 2 ou 3!\n");
-                continue;
-            }
-            break;
-        }
 
-        if (resposta == 2) printf("Correta!\n");
-        else printf("Errada!\n");
+            if(resp == corretas[i])
+                printf("Correto!\n");
+            else
+                printf("Incorreto! Resposta: %d\n", corretas[i]);
 
-        esperarEnter();
-
-        printf("Capital da Franca?\n1)Berlim 2)Madrid 3)Paris\n: ");
-
-        while (1) {
-            if (scanf("%d", &resposta) != 1) {
-                printf("Invalido!\n");
-                limparBuffer();
-                continue;
-            }
-            if (resposta < 1 || resposta > 3) {
-                printf("Digite 1, 2 ou 3!\n");
-                continue;
-            }
-            break;
-        }
-
-        if (resposta == 3) printf("Correta!\n");
-        else printf("Errada!\n");
-
-        esperarEnter();
-
-        printf("Jogar dnv? (s/n): ");
-        scanf(" %c", &jogar);
-        limparBuffer();
-
-    } while (jogar == 's' || jogar == 'S');
-}
-
-// JOGO 2
-void cobraNaCaixa() {
-    int caixas[5] = {0};
-    int botao = rand() % 5;
-    int cobra;
-
-    do { cobra = rand() % 5; } while (cobra == botao);
-
-    while (1) {
-        int escolha;
-
-        printf("Escolha caixa (1-5): ");
-
-        if (scanf("%d", &escolha) != 1) {
-            printf("Invalido!\n");
             limparBuffer();
-            continue;
         }
 
+        printf("\n1-Jogar novamente | 0-Menu: ");
+        scanf("%d",&jogar);
         limparBuffer();
 
-        if (escolha < 1 || escolha > 5 || caixas[escolha-1]) {
-            printf("Invalido!\n");
-            continue;
-        }
-
-        escolha--;
-        caixas[escolha] = 1;
-
-        if (escolha == botao) {
-            printf("GANHOU!\n");
-            break;
-        } else if (escolha == cobra) {
-            printf("PERDEU!\n");
-            break;
-        } else {
-            printf("Vazio...\n");
-        }
-    }
-
-    esperarEnter();
+    } while(jogar==1);
 }
 
-// JOGO 3
+void cobraNaCaixa() {
+    char nomes[7][20]={"Ramses","Cleopatra","Anubis","Horus","Osiris","Isis","Tut"};
+    int j1,j2,turno,botao,cobra,caixas[5],op,i;
+
+    do {
+        for(i=0;i<5;i++) caixas[i]=0;
+
+        printf("\nJogador 1 (1-7): ");
+        scanf("%d",&j1);
+
+        printf("Jogador 2 (1-7 diferente): ");
+        scanf("%d",&j2);
+        limparBuffer();
+
+        turno = rand()%2;
+        botao = rand()%5;
+
+        do { cobra=rand()%5; } while(cobra==botao);
+
+        while(1){
+            int esc;
+
+            printf("\nVez de %s\n",
+                turno==0?nomes[j1-1]:nomes[j2-1]);
+
+            printf("Escolha caixa (1-5): ");
+
+            if(scanf("%d",&esc)!=1){
+                printf("Invalido!\n");
+                limparBuffer();
+                continue;
+            }
+
+            if(esc<1 || esc>5 || caixas[esc-1]){
+                printf("Invalido!\n");
+                continue;
+            }
+
+            esc--;
+
+            if(esc==botao){
+                printf("VENCEU!\n");
+                break;
+            }
+            else if(esc==cobra){
+                printf("PERDEU!\n");
+                break;
+            }
+            else{
+                printf("Vazio...\n");
+                caixas[esc]=1;
+                turno=1-turno;
+            }
+        }
+
+        printf("\n1-Jogar novamente | 0-Menu: ");
+        scanf("%d",&op);
+        limparBuffer();
+
+    } while(op==1);
+}
+
 #define MAX 5
 
 typedef struct {
@@ -124,97 +152,99 @@ typedef struct {
     int ativa;
 } Gousma;
 
-int tem(Gousma g[2]) {
+int tem(Gousma g[2]){
     return g[0].ativa || g[1].ativa;
 }
 
-void verificar(Gousma g[2]) {
+void verificar(Gousma g[2]){
     int i;
-    for (i = 0; i < 2; i++) {
-        if (g[i].furia > MAX) {
-            g[i].ativa = 0;
+    for(i=0;i<2;i++){
+        if(g[i].ativa && g[i].furia>MAX){
+            g[i].ativa=0;
             printf("Explodiu!\n");
         }
     }
 }
 
-void gousmasWar() {
-    Gousma p1[2] = {{1,1},{1,1}};
-    Gousma p2[2] = {{1,1},{1,1}};
-    int turno = 1;
+void gousmasWar(){
+    Gousma p1[2]={{1,1},{1,1}};
+    Gousma p2[2]={{1,1},{1,1}};
+    int turno=1, op;
 
-    while (tem(p1) && tem(p2)) {
-        int op, a, t;
+    do{
+        while(tem(p1)&&tem(p2)){
+            int a,t;
 
-        printf("\nJ1[%d,%d] J2[%d,%d]\n",
-        p1[0].furia, p1[1].furia,
-        p2[0].furia, p2[1].furia);
+            printf("\nJ1[%d,%d] J2[%d,%d]\n",
+                p1[0].furia,p1[1].furia,
+                p2[0].furia,p2[1].furia);
 
-        printf("Jogador %d (1 atacar / 2 passar): ", turno);
+            printf("Jogador %d\n1-Atacar\n2-Dividir\nEscolha: ",turno);
 
-        if (scanf("%d", &op) != 1) {
-            printf("Invalido!\n");
+            if(scanf("%d",&op)!=1){
+                printf("Invalido!\n");
+                limparBuffer();
+                continue;
+            }
+
+            printf("Origem (1-2) e Alvo (1-2): ");
+
+            if(scanf("%d %d",&a,&t)!=2){
+                printf("Invalido!\n");
+                limparBuffer();
+                continue;
+            }
+
             limparBuffer();
-            continue;
+
+            if(a<1 || a>2 || t<1 || t>2){
+                printf("Invalido!\n");
+                continue;
+            }
+
+            a--; t--;
+
+            Gousma *meu = (turno==1)?p1:p2;
+            Gousma *inimigo = (turno==1)?p2:p1;
+
+            if(op==1){
+                inimigo[t].furia += meu[a].furia;
+                verificar(inimigo);
+            }
+            else if(op==2){
+                if(meu[a].furia>1){
+                    meu[a].furia--;
+                    meu[1-a].furia++;
+                    meu[1-a].ativa=1;
+                    verificar(meu);
+                }
+            }
+
+            turno = (turno==1)?2:1;
         }
 
-        printf("Origem e alvo (1-2): ");
+        if(tem(p1)) printf("J1 venceu!\n");
+        else printf("J2 venceu!\n");
 
-        if (scanf("%d %d", &a, &t) != 2) {
-            printf("Invalido!\n");
-            limparBuffer();
-            continue;
-        }
-
+        printf("\n1-Reiniciar | 0-Menu: ");
+        scanf("%d",&op);
         limparBuffer();
 
-        if (a < 1 || a > 2 || t < 1 || t > 2) {
-            printf("Invalido!\n");
-            continue;
-        }
-
-        a--; t--;
-
-        if (turno == 1) {
-            if (op == 1) p2[t].furia += 2;
-            verificar(p2);
-            turno = 2;
-        } else {
-            if (op == 1) p1[t].furia += 2;
-            verificar(p1);
-            turno = 1;
-        }
-    }
-
-    if (tem(p1)) printf("J1 venceu!\n");
-    else printf("J2 venceu!\n");
-
-    esperarEnter();
+    }while(op==1);
 }
 
-// MAIN
-int main() {
+int main(){
     srand(time(NULL));
     int op;
 
-    do {
-        menu();
+    do{
+        op = menu();
 
-        if (scanf("%d", &op) != 1) {
-            printf("Invalido!\n");
-            limparBuffer();
-            continue;
-        }
+        if(op==1) perguntas();
+        else if(op==2) cobraNaCaixa();
+        else if(op==3) gousmasWar();
 
-        limparBuffer();
-
-        if (op == 1) perguntas();
-        else if (op == 2) cobraNaCaixa();
-        else if (op == 3) gousmasWar();
-        else if (op == 4) printf("Saindo...\n");
-        else printf("Invalido!\n");
-
-    } while (op != 4);
+    }while(op!=4);
 
     return 0;
 }
